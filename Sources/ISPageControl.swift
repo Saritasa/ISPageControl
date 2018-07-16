80517797a73bd44584b1c76819545a9c6d280c7b
Alex Trukhin <troohin@gmail.com>
2018-07-16 17:58:43 +0700

    Fix the bug

--- Sources/ISPageControl.swift
+++ Sources/ISPageControl.swift
@@ -193,7 +193,7 @@ private extension ISPageControl {
             $0.element.backgroundColor = $0.offset == currentPage ? currentPageTintColor.cgColor : inactiveTintColor.withAlphaComponent(inactiveTransparency).cgColor
         }
         
-        guard numberOfPages > limit else {
+        guard numberOfPages > 0 else {
             return
         }
         
